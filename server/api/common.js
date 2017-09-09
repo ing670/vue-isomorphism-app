4a0d34bd0ba86d40a7f968593f251f290ee7b001
unknown <ing670@163.com>
2017-09-09 19:08:04 +0800

    add upload

--- server/api/common.js
+++ server/api/common.js
@@ -2,10 +2,11 @@
  * Created by sdsd on 2017/9/9.
  */
 var multer  = require('multer')
+const path  = require('path')
 var storage = multer.diskStorage({
-    //设置上传后文件路径，uploads文件夹会自动创建。
+    //设置上传后文件路径，uploads文件夹不会自动创建。
     destination: function (req, file, cb) {
-        cb(null, __dirname+'/../../front/public/uploads')
+        cb(null, path.join(__dirname,'../../front/public/upload/'));
     },
     //给上传文件重命名，获取添加后缀名
     filename: function (req, file, cb) {
@@ -23,8 +24,7 @@ module.exports = [
         mid:upload.single('cover'),
         action: "upload",
         callback: function (req, res) {
-            console.log(req.file)
-            res.json(req.file)
+            res.json({code:0,cover:`/public/upload/${req.file.filename}`})
         }
     }
 ]
